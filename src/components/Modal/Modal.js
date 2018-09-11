0c8f15d4b275214bae71cc7148516ab1862f9a44
dim3n21 <dima.andrianov@gmail.com>
2018-09-11 15:30:04 -0700

    Implement the Transition component inside the Modal component and delete it from the main App component.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
 const modal = (props) => {
 
-  const cssClass = ['Modal', props.show === 'entering' ? 'ModalIsOpen' : props.show === 'exiting' ? 'ModalIsClosed' : null];
+
   return (
-    <div className={cssClass.join(' ')}>
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
+    <Transition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={300}>
+      {state => {
+        const cssClass = ['Modal', state === 'entering' ? 'ModalIsOpen' : state === 'exiting' ? 'ModalIsClosed' : null];
+        return (
+          <div className={cssClass.join(' ')}>
+              <h1>A Modal</h1>
+              <button className="Button" onClick={props.closed}>Dismiss</button>
+          </div>
+        )
+      }}
+
+    </Transition>
   )
 };
 

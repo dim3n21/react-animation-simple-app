f4a6b92462c37015d3cae603c24b59ed2fb57d97
dim3n21 <dima.andrianov@gmail.com>
2018-09-11 13:44:59 -0700

    Set up a project. Add css code for Modal and Backdrop components. Implement showModal-closeModal functionaliy. Implement button functionality.

---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Backdrop.css';
+
+const backdrop = (props) => {
+
+  const cssClass = ['Backdrop', props.show ? 'BackdropIsOpen' : 'BackdropIsClosed'];
+
+  return (
+    <div className={cssClass.join(' ')}></div>
+  );
+};
+
+export default backdrop;
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Modal.css';
+
+const modal = (props) => {
+
+  const cssClass = ['Modal', props.show ? 'ModalIsOpen' : 'ModalIsClosed'];
+  return (
+    <div className={cssClass.join(' ')}>
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>Dismiss</button>
+    </div>
+  )
+};
+
+export default modal;
